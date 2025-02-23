2b98a6a6a9621cee0ad676a917885b05146e271a
justinbeaver <13700367+justinbeaver@users.noreply.github.com>
2025-02-24 01:37:31 +0200

    Create Home page component

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,45 @@
+import Btn from "@/components/Btn/Btn";
+import useMediaQueries from "@/hooks/useMediaQueries";
+
+import s from "./Home.module.scss";
+
+const Home = () => {
+  return (
+    <div className={s.page}>
+      <div className="wrapper">
+        <div className={s.content}>
+          <Header />
+          <ExploreButton />
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Home;
+
+const Header = () => {
+  return (
+    <header className={s.header}>
+      <h1 className={s.title}>
+        SO, YOU WANT TO TRAVEL TO
+        <span className={s.emphasized}>SPACE</span>
+      </h1>
+      <p className={s.subtitle}>
+        Let’s face it; if you want to go to space, you might as well genuinely
+        go to outer space and not hover kind of on the edge of it. Well sit
+        back, and relax because we’ll give you a truly out of this world
+        experience!
+      </p>
+    </header>
+  );
+};
+
+const ExploreButton = () => {
+  const { isSm } = useMediaQueries();
+
+  return (
+    <Btn variant="primary" size={isSm ? "lg" : "md"}>
+      EXPLORE
+    </Btn>
+  );
+};
