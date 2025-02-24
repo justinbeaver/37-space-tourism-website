bb6369634331c5ee8988e79f460dd105b6095ba2
justinbeaver <13700367+justinbeaver@users.noreply.github.com>
2025-02-24 02:02:59 +0200

    Create PageBackground component

---
+++ src/components/PageBackground/PageBackground.jsx
@@ -0,0 +1,18 @@
+import PropTypes from "prop-types";
+import { cva } from "class-variance-authority";
+
+import config from "./PageBackground.config";
+
+import s from "./PageBackground.module.scss";
+
+const pageBackgroundClass = cva(s.page, config);
+
+const PageBackground = ({ variant, children }) => {
+  return <div className={pageBackgroundClass({ variant })}>{children}</div>;
+};
+export default PageBackground;
+
+PageBackground.propTypes = {
+  variant: PropTypes.oneOf(Object.keys(config.variants.variant)),
+  children: PropTypes.node,
+};
