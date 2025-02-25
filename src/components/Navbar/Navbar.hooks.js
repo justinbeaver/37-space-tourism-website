d9f5ea138444c86efda8eeb619df6836af54a340
justinbeaver <13700367+justinbeaver@users.noreply.github.com>
2025-02-25 14:13:32 +0200

    Update useNavbarHeight to use useElementSize hook

--- src/components/Navbar/Navbar.hooks.js
+++ src/components/Navbar/Navbar.hooks.js
@@ -1,39 +1,17 @@
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 
-import useDebounce from "@/hooks/useDebounce";
+import useElementSize from "@/hooks/useElementSize";
 import { setCssCustomProperty } from "./Navbar.utils";
 
 /**
  * Gets navbar's height and sets it as css custom property on the root element
  */
 export const useNavbarHeight = () => {
-  const navbarRef = useRef(null);
-
-  // Wrap with callback
-  const updateHeight = () => {
-    if (!navbarRef.current) return;
-
-    // console.log("updateHeight");
-
-    const navbarHeight = navbarRef.current.offsetHeight;
-
-    setCssCustomProperty("--navbar-height", `${navbarHeight}px`);
-  };
-
-  const debouncedUpdateHeight = useDebounce(updateHeight, 100);
+  const { ref, size } = useElementSize();
 
   useEffect(() => {
-    const handleResize = () => {
-      debouncedUpdateHeight();
-    };
+    setCssCustomProperty("--navbar-height", `${size.height}px`);
+  }, [size.height]);
 
-    // Sets initial height when navbar mounts
-    updateHeight();
-
-    window.addEventListener("resize", handleResize);
-
-    return () => window.removeEventListener("resize", handleResize);
-  }, [debouncedUpdateHeight]);
-
-  return { navbarRef };
+  return { navbarRef: ref };
 };
