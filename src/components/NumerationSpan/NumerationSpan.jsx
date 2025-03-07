94b97963c3afe6c781535939e274b854313b4434
justinbeaver <13700367+justinbeaver@users.noreply.github.com>
2025-03-08 01:58:18 +0200

    Create NumerationSpan

---
+++ src/components/NumerationSpan/NumerationSpan.jsx
@@ -0,0 +1,33 @@
+import PropTypes from "prop-types";
+import { cva } from "class-variance-authority";
+
+import config from "./NumerationSpan.config";
+
+import s from "./NumerationSpan.module.scss";
+
+const numerationSpanClass = cva(s.numerationSpan, config);
+
+const NumerationSpan = ({
+  margin = "right",
+  tone = "normal",
+  className = "",
+  children,
+  ...rest
+}) => {
+  return (
+    <span
+      className={`${numerationSpanClass({ margin, tone })} ${className}`}
+      {...rest}
+    >
+      {children}
+    </span>
+  );
+};
+export default NumerationSpan;
+
+NumerationSpan.propTypes = {
+  margin: PropTypes.oneOf(config.variants.margin),
+  tone: PropTypes.oneOf(config.variants.tone),
+  className: PropTypes.string,
+  children: PropTypes.node,
+};
