7b2a3eebe4a23efec1377d143bb31e8d3626046b
justinbeaver <13700367+justinbeaver@users.noreply.github.com>
2025-03-07 02:27:59 +0200

    Update ImageSlider with row-reverse option

--- src/components/ImageSlider/ImageSlider.config.js
+++ src/components/ImageSlider/ImageSlider.config.js
@@ -4,6 +4,7 @@ const config = {
   variants: {
     direction: {
       horizontal: s.imageSlider__horizontal,
+      "horizontal-reverse": s.imageSlider__horizontalReverse,
       vertical: s.imageSlider__vertical,
       "vertical-reverse": s.imageSlider__verticalReverse,
     },
