97a6aa6e7c0986289a5746b075e7c9c4975702df
justinbeaver <13700367+justinbeaver@users.noreply.github.com>
2025-03-08 20:15:28 +0200

    Update Home style

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import Btn from "@/components/Btn/Btn";
 import PageBackground from "@/components/PageBackground/PageBackground";
+import PagePadding from "@/components/PagePadding/PagePadding";
 import { useMediaQueriesContext } from "@/context/mediaQueriesContext";
 
 import s from "./Home.module.scss";
@@ -7,12 +8,12 @@ import s from "./Home.module.scss";
 const Home = () => {
   return (
     <PageBackground variant="home">
-      <div className="wrapper">
+      <PagePadding>
         <div className={s.content}>
           <Header />
           <ExploreButton />
         </div>
-      </div>
+      </PagePadding>
     </PageBackground>
   );
 };
@@ -22,10 +23,10 @@ const Header = () => {
   return (
     <header className={s.header}>
       <h1 className={s.title}>
-        SO, YOU WANT TO TRAVEL TO
+        <span>SO, YOU WANT TO TRAVEL TO</span>
         <span className={s.emphasized}>SPACE</span>
       </h1>
-      <p className={s.subtitle}>
+      <p className={s.description}>
         Let’s face it; if you want to go to space, you might as well genuinely
         go to outer space and not hover kind of on the edge of it. Well sit
         back, and relax because we’ll give you a truly out of this world
@@ -39,8 +40,10 @@ const ExploreButton = () => {
   const { isSm } = useMediaQueriesContext();
 
   return (
-    <Btn variant="primary" size={isSm ? "lg" : "md"}>
-      EXPLORE
-    </Btn>
+    <div className={s.btnWrapper}>
+      <Btn variant="primary" size={isSm ? "lg" : "md"}>
+        EXPLORE
+      </Btn>
+    </div>
   );
 };
