95e643a96b3dd827f1b7b5e8a15368771c8edc90
justinbeaver <13700367+justinbeaver@users.noreply.github.com>
2025-03-09 16:01:10 +0200

    Update Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,36 +1,66 @@
+import { Link } from "react-router-dom";
+
+import Wrapper from "@/components/Wrapper/Wrapper";
 import DecorationLine from "../DecorationLine/DecorationLine";
 import NavItem from "@/components/NavItem/NavItem";
 import NumerationSpan from "../NumerationSpan/NumerationSpan";
 import { useNavbarHeight } from "./Navbar.hooks";
 import { useMediaQueriesContext } from "@/context/mediaQueriesContext";
-import Logo from "@/assets/shared/logo.svg?react";
+import LogoIcon from "@/assets/shared/logo.svg?react";
 import content from "./Navbar.content.json";
 
 import s from "./Navbar.module.scss";
 
 const Navbar = () => {
-  const { isMd } = useMediaQueriesContext();
   const { navbarRef } = useNavbarHeight();
 
   return (
     <header ref={navbarRef} className={s.header}>
-      <Logo className={s.logo} />
-      {isMd && <DecorationLine className={s.decorationLine} />}
-      <nav className={s.nav}>
-        <ul className={s.list}>
-          {content.nav.map(({ label, href }, index) => (
-            <li key={index} className={s.listItem}>
-              <NavItem to={href}>
-                <NumerationSpan margin="none">
-                  {String(index).padStart(2, "0")}
-                </NumerationSpan>
-                {label}
-              </NavItem>
-            </li>
-          ))}
-        </ul>
-      </nav>
+      <Wrapper size="lg" padding="none">
+        <div className={s.headerInner}>
+          <Logo />
+          <Decoration />
+          <Nav />
+        </div>
+      </Wrapper>
     </header>
   );
 };
 export default Navbar;
+
+const Logo = () => {
+  return (
+    <Link className={s.logo} to="/" aria-label="home">
+      <LogoIcon aria-label="logo icon" />
+    </Link>
+  );
+};
+
+const Decoration = () => {
+  const { isMd } = useMediaQueriesContext();
+
+  return isMd ? (
+    <DecorationLine className={s.decorationLine} aria-hidden={true} />
+  ) : null;
+};
+
+const Nav = () => {
+  const { nav } = content;
+
+  return (
+    <nav className={s.nav}>
+      <ul className={s.list}>
+        {nav.map(({ label, href }, index) => (
+          <li key={index} className={s.listItem}>
+            <NavItem to={href}>
+              <NumerationSpan margin="none">
+                {String(index).padStart(2, "0")}
+              </NumerationSpan>
+              <span>{label}</span>
+            </NavItem>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
