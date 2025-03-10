c674c5a887ba9ecf0ac2e643c98316a5990b1e6a
justinbeaver <13700367+justinbeaver@users.noreply.github.com>
2025-03-10 13:39:08 +0200

    Create useClickAway.js

---
+++ src/hooks/useClickAway.js
@@ -0,0 +1,22 @@
+import { useEffect, useRef } from "react";
+
+const useClickAway = (callback) => {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const handleClickAway = (e) => {
+      if (ref.current && !ref.current.contains(e.target)) {
+        callback();
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickAway);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickAway);
+    };
+  }, [callback]);
+
+  return { ref };
+};
+export default useClickAway;
