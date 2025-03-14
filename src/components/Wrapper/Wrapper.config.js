3855845df93b47b5e93cf7687a8bc7a404110220
justinbeaver <13700367+justinbeaver@users.noreply.github.com>
2025-03-14 13:06:43 +0200

    Replace old wrapper with new wrapper

--- src/components/Wrapper/Wrapper.config.js
+++ src/components/Wrapper/Wrapper.config.js
@@ -1,15 +1,31 @@
+import { cva } from "class-variance-authority";
+
+import { generateSizeRanges } from "./Wrapper.utils";
+
 import s from "./Wrapper.module.scss";
 
-const config = {
+const BASE_CLASS = "wrapper";
+
+const sizes = ["sm", "md", "lg", "full"];
+
+const sizeRanges = generateSizeRanges(BASE_CLASS, sizes);
+
+export const config = {
   variants: {
     size: {
-      md: s.wrapper__md,
-      lg: s.wrapper__lg,
+      sm: [s[`${BASE_CLASS}__start--sm`], s[`${BASE_CLASS}__end--sm`]],
+      md: [s[`${BASE_CLASS}__start--md`], s[`${BASE_CLASS}__end--md`]],
+      lg: [s[`${BASE_CLASS}__start--lg`], s[`${BASE_CLASS}__end--lg`]],
+      full: [s[`${BASE_CLASS}__start--full`], s[`${BASE_CLASS}__end--full`]],
+      ...sizeRanges,
     },
-    padding: {
-      none: null,
-      md: s["wrapper__padding--md"],
+    pad: {
+      none: s[`${BASE_CLASS}__pad--none`],
+      sm: null,
+      md: s[`${BASE_CLASS}__pad--md`],
+      lg: s[`${BASE_CLASS}__pad--lg`],
     },
   },
 };
-export default config;
+
+export const wrapperClass = cva(s[BASE_CLASS], config);
--- src/components/Wrapper/Wrapper.jsx
+++ src/components/Wrapper/Wrapper.jsx
@@ -1,24 +1,25 @@
 import PropTypes from "prop-types";
-import { cva } from "class-variance-authority";
 
-import config from "./Wrapper.config";
+import { wrapperClass, config } from "./Wrapper.config";
 
-import s from "./Wrapper.module.scss";
-
-const wrapperClass = cva(s.wrapper, config);
-
-const Wrapper = ({ size = "md", padding = "md", className = "", ...rest }) => {
+const Wrapper = ({
+  size = "md",
+  pad = "md",
+  className = "",
+  children,
+  ...rest
+}) => {
   return (
-    <div
-      className={`${wrapperClass({ size, padding })} ${className}`}
-      {...rest}
-    />
+    <div className={`${wrapperClass({ size, pad })} ${className}`} {...rest}>
+      {children}
+    </div>
   );
 };
 export default Wrapper;
 
 Wrapper.propTypes = {
-  size: PropTypes.oneOf(Object.keys(config.variants.size)),
-  padding: PropTypes.oneOf(Object.keys(config.variants.padding)),
+  size: PropTypes.oneOf(Object.keys(config.variants.size)).isRequired,
+  pad: PropTypes.oneOf(Object.keys(config.variants.pad)).isRequired,
   className: PropTypes.string,
+  children: PropTypes.node,
 };
